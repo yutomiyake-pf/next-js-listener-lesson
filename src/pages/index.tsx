f5dc7228dcd5920ae2dcfcb81ddd34376129bbd0
y.miyake <dokyuto106412@gmail.com>
2023-09-16 22:41:25 +0900

    画像の切り替え処理

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next';
 import Image from 'next/image';
 
+import { MouseEvent, useState } from 'react';
 import styles from '@/styles/imageList.module.scss';
 
 const IndexPage: NextPage = () => {
@@ -11,26 +12,38 @@ const IndexPage: NextPage = () => {
     '/images/mainvisual4.jpg',
     '/images/mainvisual5.jpg',
   ];
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const onMouseEnter = (targetIndex: number) => {
+    if (!images[targetIndex]) return;
+    setCurrentImageIndex(targetIndex);
+  };
 
   return (
     <main className={styles.imageList}>
       <div className={styles.imageList__content}>
         <Image
           className={styles.imageList__bgImage}
-          src={images[0]}
+          src={images[currentImageIndex]}
           alt={'大きい画像'}
           width={960}
           height={540}
+          loading='lazy'
         />
         <ul className={styles.imageList__smallImageLists}>
-          {images.map((image) => (
-            <li className={styles.imageList__smallImageList} key={image}>
+          {images.map((image, index) => (
+            <li
+              className={styles.imageList__smallImageList}
+              key={image}
+              onMouseEnter={() => onMouseEnter(index)}
+            >
               <Image
                 className={styles.imageList__smallImage}
                 src={image}
                 alt={'小さい画像'}
                 height={103}
                 width={172}
+                loading='lazy'
               />
             </li>
           ))}
