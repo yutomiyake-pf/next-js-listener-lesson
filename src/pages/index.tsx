a2c405637293e3497e12148084a39adb744ae24d
y.miyake <dokyuto106412@gmail.com>
2023-09-16 22:24:58 +0900

    画像を配置するところまで

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,42 @@
 import { NextPage } from 'next';
+import Image from 'next/image';
+
+import styles from '@/styles/imageList.module.scss';
 
 const IndexPage: NextPage = () => {
+  const images = [
+    '/images/mainvisual1.jpg',
+    '/images/mainvisual2.jpg',
+    '/images/mainvisual3.jpg',
+    '/images/mainvisual4.jpg',
+    '/images/mainvisual5.jpg',
+  ];
+
   return (
-    <div>
-      <div>
-        <h1>index</h1>
+    <main className={styles.imageList}>
+      <div className={styles.imageList__content}>
+        <Image
+          className={styles.imageList__bgImage}
+          src={images[0]}
+          alt={''}
+          width={960}
+          height={540}
+        />
+        <ul className={styles.imageList__smallImageLists}>
+          {images.map((image) => (
+            <li className={styles.imageList__smallImageList} key={image}>
+              <Image
+                className={styles.imageList__smallImage}
+                src={image}
+                alt={''}
+                height={103}
+                width={172}
+              />
+            </li>
+          ))}
+        </ul>
       </div>
-    </div>
+    </main>
   );
 };
 
